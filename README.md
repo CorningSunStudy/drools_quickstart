ba5aef2b595de064fe772c20bd5d405e8ef0458f
Corning Sun <corningsun@163.com>
2020-10-23 16:12:13 +0800

    P47 05-Drools高级语法（Query查询_带有查询参数的查询）

--- README.md
+++ README.md
@@ -6,6 +6,12 @@
 
 https://www.bilibili.com/video/BV1Pa4y1a7u6?p=12
 
+## 文档地址
+
+> forked from HappySnailSunshine/JavaInterview
+
+https://github.com/corningsun/JavaInterview/blob/master/docs/Drools.md
+
 ## 规则引擎构成
 
 ![](docs/img/规则引擎构成.png)
--- src/test/java/com/corning/drools/entity/QueryTest.java
+++ src/test/java/com/corning/drools/entity/QueryTest.java
@@ -1,6 +1,7 @@
 package com.corning.drools.entity;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
 import org.junit.Test;
 import org.kie.api.runtime.rule.QueryResults;
 import org.kie.api.runtime.rule.QueryResultsRow;
@@ -8,8 +9,10 @@ import org.kie.api.runtime.rule.QueryResultsRow;
 @Slf4j
 public class QueryTest extends DroolsBaseTest {
 
-    @Test
-    public void query1() {
+    @Before
+    public void setUP() {
+        super.setUP();
+
         final Student student1 = new Student();
         student1.setAge(1);
         student1.setName("student1");
@@ -19,24 +22,43 @@ public class QueryTest extends DroolsBaseTest {
         student2.setAge(2);
         student2.setName("student2");
         kieSession.insert(student2);
+    }
 
-        final QueryResults queryResults1 = kieSession.getQueryResults("query_student_age_less_10");
-        log.info("queryResults1.size()={}", queryResults1.size());
+    @Test
+    public void query1() {
+        final QueryResults queryResults = kieSession.getQueryResults("query_student_age_less_10");
+        log.info("queryResults.size()={}", queryResults.size());
 
-        for (QueryResultsRow queryResultsRow : queryResults1) {
+        for (QueryResultsRow queryResultsRow : queryResults) {
             final Student student = (Student) queryResultsRow.get("$student");
             log.info("student={}", student);
         }
 
         /*
         [main] INFO com.corning.drools.entity.DroolsBaseTest - Fri Oct 23 15:48:07 CST 2020 - kieSession init
-        [main] INFO com.corning.drools.entity.QueryTest - queryResults1.size()=2
+        [main] INFO com.corning.drools.entity.QueryTest - queryResults.size()=2
         [main] INFO com.corning.drools.entity.QueryTest - student=Student(id=0, name=student1, age=1)
         [main] INFO com.corning.drools.entity.QueryTest - student=Student(id=0, name=student2, age=2)
         [main] INFO com.corning.drools.entity.DroolsBaseTest - Fri Oct 23 15:48:07 CST 2020 - kieSession dispose
          */
-
     }
 
+    @Test
+    public void query2() {
+        final QueryResults queryResults = kieSession.getQueryResults("query_student_by_name", "student2");
+        log.info("queryResults.size()={}", queryResults.size());
+
+        for (QueryResultsRow queryResultsRow : queryResults) {
+            final Student student = (Student) queryResultsRow.get("$student");
+            log.info("student={}", student);
+        }
+
+        /*
+        [main] INFO com.corning.drools.entity.DroolsBaseTest - Fri Oct 23 15:57:30 CST 2020 - kieSession init
+        [main] INFO com.corning.drools.entity.QueryTest - queryResults.size()=1
+        [main] INFO com.corning.drools.entity.QueryTest - student=Student(id=0, name=student2, age=2)
+        [main] INFO com.corning.drools.entity.DroolsBaseTest - Fri Oct 23 15:57:30 CST 2020 - kieSession dispose
+         */
+    }
 
 }
